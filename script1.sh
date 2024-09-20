8e70af45b0227f8d73dbc5e14f44dd67eb6f6419
zzucch <nikitamilovatskiy@gmail.com>
2024-09-20 16:19:59 +0300

    feat: add ipv4 info

--- script1.sh
+++ script1.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-get_network_info() {
+get_interface_info() {
   echo "Net interface info:"
+
   for interface in $(ip -brief address show | awk '{print $1;}'); do
     echo "Interface: $interface"
+
     echo "Model: $(ethtool -i "$interface" | grep 'driver' | awk '{print $2}')"
     echo "Speed: $(ethtool "$interface" | grep 'Speed' | awk '{print $2}')"
     echo "Duplex mode: $(ethtool "$interface" | grep 'Duplex' | awk '{print $2}')"
@@ -13,4 +15,47 @@ get_network_info() {
   done
 }
 
-get_network_info
+get_ipv4_info() {
+  echo "IPv4 info:"
+
+  for interface in $(ip -brief address show | awk '{print $1;}'); do
+    echo "Interface: $interface"
+
+    ip -4 addr show |
+      grep inet |
+      awk '{print $2}' |
+      while read -r line; do
+        echo "IP/mask: $line"
+      done
+
+    echo -e "\nDefault gateway:"
+    ip route | grep default | awk '{print $3}'
+
+    echo -e "\nDNS servers:"
+    grep nameserver /etc/resolv.conf | awk '{print $2}'
+    echo ""
+  done
+}
+
+opts=(interface_info ipv4_info quit)
+
+while true; do
+  PS3="Select from following: "
+
+  select opt in "${opts[@]}"; do
+    case $opt in
+    interface_info)
+      get_interface_info
+      ;;
+    ipv4_info)
+      get_ipv4_info
+      ;;
+    quit)
+      exit 0
+      ;;
+    *)
+      error "Unexpected expression $opt"
+      ;;
+    esac
+  done
+done
